1f9aa84900e5e86c0eb0401393e6162eb7b33d9c
DeadlyWeapon-beep90 <66587312+DeadlyWeapon-beep90@users.noreply.github.com>
2020-06-08 12:31:47 +0545

    Update SQLi.py

--- SQLi.py
+++ SQLi.py
@@ -15,7 +15,7 @@ def checkvpn():
   print("")
 def banner():
  os.system("clear")
- os.system("toilet -fmono12 -F gay HACKER BANO CHUITYA NAHI")
+ os.system("toilet -fmono12 -F gay DEAD")
 print('''\033[1;34m_
 ⠀⠀⠀⢀⣾⣿⣿⣿⠿⠿⠟⠻⠿⢿⣿⣿⣿⡆
 ⠀⠀⠀⢰⣿⣿⡿⠂⠀⠀⠀⠀⠀⠀⠈⠉⢻⡇ ⠀⠀⠀⠀⠀
